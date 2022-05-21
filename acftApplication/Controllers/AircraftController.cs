ed1cbe4f5233ad61fdb59c0e5b35e511d3246a07
Serega800 <svs3107@gmail.com>
2022-05-21 23:04:06 +0500

    added Aircraft View Index. Controller coded

---
+++ acftApplication/Controllers/AircraftController.cs
@@ -0,0 +1,35 @@
+﻿using acftApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acftApplication.Controllers
+{
+    public class AircraftController : Controller
+    {
+        private readonly DataBaseContext _dbContext;
+        public AircraftController(DataBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dbContext.Aircrafts.ToListAsync());
+        }
+        [HttpGet]
+        public IActionResult AddAircraft()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddAircraft(Aircraft acft)
+        {
+            await _dbContext.Aircrafts.AddAsync(acft);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
