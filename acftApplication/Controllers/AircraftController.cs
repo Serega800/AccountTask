8a2a79ae5ce9831541f4f774391339f919d83933
Serega800 <svs3107@gmail.com>
2022-05-22 00:05:41 +0500

    update aircraft functionality fixed

--- acftApplication/Controllers/AircraftController.cs
+++ acftApplication/Controllers/AircraftController.cs
@@ -25,22 +25,30 @@ namespace acftApplication.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> AddAircraft(Aircraft acft)
+        public async Task<IActionResult> AddAircraft(Aircraft aircraft)
         {
-            await _dbContext.Aircrafts.AddAsync(acft);
+            await _dbContext.Aircrafts.AddAsync(aircraft);
             await _dbContext.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
         [HttpGet]
-        public async Task<IActionResult> UpdateAircraft(int acftId)
+        public async Task<IActionResult> UpdateAircraft(int aircraftId)
         {
-            Aircraft acft = await _dbContext.Aircrafts.FindAsync(acftId);
+            Aircraft acft = await _dbContext.Aircrafts.FindAsync(aircraftId);
             return View(acft);
         }
         [HttpPost]
-        public async Task<IActionResult> UpdateAircraft(Aircraft acft)
+        public async Task<IActionResult> UpdateAircraft(Aircraft aircraft)
         {
-            _dbContext.Aircrafts.Update(acft);
+            _dbContext.Aircrafts.Update(aircraft);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteAircraft(int aircraftId)
+        {
+            Aircraft acft = await _dbContext.Aircrafts.FindAsync(aircraftId);
+            _dbContext.Aircrafts.Remove(acft);
             await _dbContext.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
