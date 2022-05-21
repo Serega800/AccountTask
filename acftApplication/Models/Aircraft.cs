42ab2fd902a416d065a2fa96b842d50bcb1454f1
Serega800 <svs3107@gmail.com>
2022-05-21 22:41:38 +0500

    Добавьте файлы проекта.

---
+++ acftApplication/Models/Aircraft.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acftApplication.Models
+{
+    public class Aircraft
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
---
+++ acftApplication/Models/DataBaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace acftApplication.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<Aircraft> Aircrafts { get; set; }
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
