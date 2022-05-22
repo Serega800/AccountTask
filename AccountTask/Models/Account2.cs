e97d5cda85901150bf8f6ef5e25118649fd4101d
Serega800 <svs3107@gmail.com>
2022-05-23 00:04:51 +0500

    models Account2 and Address added

---
+++ AccountTask/Models/Account2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTask.Models
+{
+    public class Account2 : Address
+    {    
+        [Key]
+        public Guid Id { get; private set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public Address PropertyAddress { get; set; }
+        [Required]
+        public double Area { get; set; }
+        [Required]
+        public int ResidentsCount { get; set; }
+        [Required]
+        public DateTime OpeningDate { get; }
+        public DateTime ClosingDate { get; }                    
+    }
+}
---
+++ AccountTask/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTask.Models
+{
+    public class Address
+    {
+        public string Locality { get; set; }
+        public string Street { get; set; }
+        public string Building { get; set; }
+        public string Case { get; set; }
+        public string ApartmentNumber { get; set; }        
+        public string GetAddress()
+        {
+            var address = new StringBuilder();
+
+            PropertyInfo[] properties = typeof(Address).GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (!string.IsNullOrEmpty(property.ToString()))
+                {                    
+                    address.AppendJoin(",", property.ToString());
+                }
+            }
+            return address.ToString();
+        }        
+    }
+}
