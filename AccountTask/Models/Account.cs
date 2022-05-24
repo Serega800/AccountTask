b53b3950b99de9b3136fd24c86a6db585d2ff5f5
Serega800 <svs3107@gmail.com>
2022-05-25 00:38:01 +0500

    added field AccountId to model. Migration performed.

--- AccountTask/Models/Account.cs
+++ AccountTask/Models/Account.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 namespace AccountTask.Models
 {    
     public class Account
-    {
+    {        
         public int Id { get; set; }
+
+        [Display(Name = "Номер Лицевого Счёта")]
+        public string AccountId         
+        {
+            get
+            {
+                return Id.ToString().PadLeft(10, '0');               
+            }
+        }        
 
         [Display(Name ="ФИО")]
         public string FullName { get; set; }
