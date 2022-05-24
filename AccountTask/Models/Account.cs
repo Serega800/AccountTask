44131e441a3d8d5beef3af4ab5ce2bdced9fba1c
Serega800 <svs3107@gmail.com>
2022-05-24 21:26:57 +0500

    added attributes to Account model

--- AccountTask/Models/Account.cs
+++ AccountTask/Models/Account.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AccountTask.Models
-{
+{    
     public class Account
     {
         public int Id { get; set; }
+
+        [Display(Name ="ФИО")]
         public string FullName { get; set; }
+        [Display(Name = "Населённый пункт")]
         public string Locality { get; set; }
+        [Display(Name = "Квартира")]
         public int ApartmentNumber { get; set; }
     }
 }
