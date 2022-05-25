9a498072703c050f2e371a6a88fae5b3a24c68ca
Serega800 <svs3107@gmail.com>
2022-05-25 23:48:10 +0500

    continuing validation practice

--- AccountTask/Models/Account.cs
+++ AccountTask/Models/Account.cs
@@ -29,6 +29,7 @@ namespace AccountTask.Models
         [Required(ErrorMessage = "Необходимо указать улицу")]
         public string Street { get; set; }
         [Display(Name = "Дом")]
+        //[RegularExpression(@"/[0-9]{1,3}[0-9абвгде\/]{1,4}/i$", ErrorMessage = "Номер дома не может быть отрицательным, может содержать копус или литеру.")]
         [Required(ErrorMessage = "Необходимо указать номер дома")]
         public string Building { get; set; }
         [Display(Name = "Номер квартиры")]
